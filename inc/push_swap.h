cccb4db062a548fa36f06614148c2e3483327ead
Gabriel Tocha <gabriel.tocha@gmail.com>
2022-12-19 21:15:52 +0000

    Initial Setup

---
+++ inc/push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gda-cruz <gda-cruz@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 21:13:19 by gda-cruz          #+#    #+#             */
+/*   Updated: 2022/12/19 21:14:54 by gda-cruz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <error.h>
+
+#endif
